3c7d76ab910b42e50914f15907332c3a78168c11
webjjang <leelj1@nate.com>
2026-07-30 11:57:46 +0900

    Pacs 데이터를 모두 처리하기 위한 VO 수정

--- webjjang_back/src/main/java/com/webjjang/api/pacs/vo/StudyVO.java
+++ webjjang_back/src/main/java/com/webjjang/api/pacs/vo/StudyVO.java
@@ -2,13 +2,35 @@ package com.webjjang.api.pacs.vo;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class StudyVO {
 
+    /* Orthanc Study ID */
     private String id;
-    private String patientName;
+
+    // Patient - 환자 정보
     private String patientId;
+    private String patientName;
+    private String patientSex;
+    private String patientBirthDate;
+
+    // Study - 진료 정보
+    private String studyInstanceUID;
+    private String accessionNumber;
     private String studyDate;
+    private String studyTime;
     private String studyDescription;
+    private String referringPhysicianName;
+    private String requestedProcedureDescription;
+    private String studyID;
+
+    // 기타
+    private String parentPatient;
+    private boolean stable;
+
+    // Series 정보
+    private List<String> series;
 
 }
